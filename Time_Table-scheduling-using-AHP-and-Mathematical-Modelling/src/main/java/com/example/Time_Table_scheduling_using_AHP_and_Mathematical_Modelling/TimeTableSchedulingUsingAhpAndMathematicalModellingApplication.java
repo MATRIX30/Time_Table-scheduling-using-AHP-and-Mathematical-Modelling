78e10d30ab819c8f9e87700191730e59717e785d
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2024-05-27 04:06:07 +0100

    Spring boot project setUp.

---
+++ Time_Table-scheduling-using-AHP-and-Mathematical-Modelling/src/main/java/com/example/Time_Table_scheduling_using_AHP_and_Mathematical_Modelling/TimeTableSchedulingUsingAhpAndMathematicalModellingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Time_Table_scheduling_using_AHP_and_Mathematical_Modelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableSchedulingUsingAhpAndMathematicalModellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTableSchedulingUsingAhpAndMathematicalModellingApplication.class, args);
+	}
+
+}
---
+++ Time_Table-scheduling-using-AHP-and-Mathematical-Modelling/src/test/java/com/example/Time_Table_scheduling_using_AHP_and_Mathematical_Modelling/TimeTableSchedulingUsingAhpAndMathematicalModellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Time_Table_scheduling_using_AHP_and_Mathematical_Modelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableSchedulingUsingAhpAndMathematicalModellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
