c8b4ded236544674d228d0b8d42c949663347a58
DavePhil <104580100+DavePhil@users.noreply.github.com>
2024-06-07 11:26:22 +0100

    Add false sets (waiting for true sets)

--- backend/src/main/java/com/timetablescheduling/backend/service/constraints/Constraints.java
+++ backend/src/main/java/com/timetablescheduling/backend/service/constraints/Constraints.java
@@ -1,7 +1,28 @@
 package com.timetablescheduling.backend.service.constraints;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Constraints {
     // All the constraints are method and will be added to the resolver
+
+    // Defining of sets waiting all the real sets and parameters
+
+    List<Object> students = new ArrayList<>();
+    List<Object> lecturers = new ArrayList<>();
+    List<Object> rooms = new ArrayList<>();
+    List<Object> timeslots = new ArrayList<>();
+    List<Object> filiere = new ArrayList<>();
+    List<Object> semester = new ArrayList<>();
+    List<Object> courses = new ArrayList<>();
+
+
+    Integer nbStudentForLevel;
+    Integer roomSize;
+    Integer maxNbCourseGiveByLecturer;
+
+
+
 
 
 }
