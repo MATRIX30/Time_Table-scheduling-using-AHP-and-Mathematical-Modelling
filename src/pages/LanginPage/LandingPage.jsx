0febebd22a3cdb59e98f9e82920a163573132612
tomdieu <ivantomdio@gmail.com>
2024-06-08 10:23:21 +0100

    style : updated styles

--- src/pages/LanginPage/LandingPage.jsx
+++ src/pages/LanginPage/LandingPage.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
       <Navbar/>
       <main className="flex-1">
         <section className="w-full pt-12 md:pt-24 lg:pt-32 border-y">
-          <div className="px-4 container mx-auto md:px-6 space-y-10 xl:space-y-16">
+          <div className="px-4 container mx-auto md:px-6 space-y-10 xl:space-y-16 py-5">
             <div className="grid mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
